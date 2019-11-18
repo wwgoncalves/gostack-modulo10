277383fbf441b4c6cb22be9e380fb1c798a99c04
lilo <williamcco@gmail.com>
2019-11-18 17:47:20 -0300

    developing the booking screens flow

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+
+import SelectProvider from './pages/New/SelectProvider';
+import SelectDateTime from './pages/New/SelectDateTime';
+import Confirm from './pages/New/Confirm';
 
 import Dashboard from './pages/Dashboard';
 import Profile from './pages/Profile';
@@ -18,6 +25,39 @@ export default (signedIn = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#fff',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                    // headerTitleContainerStyle: {
+                    //   justifyContent: 'center',
+                    //   //paddingLeft: 40,
+                    // },
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Book',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255, 255, 255, 0.6)"
+                  />
+                ),
+              },
+            },
             Profile,
           },
           {
@@ -28,7 +68,6 @@ export default (signedIn = false) =>
               style: {
                 backgroundColor: '#8d41a8',
                 flexGrow: 1,
-                // borderTopWidth: 0,
               },
             },
           }
